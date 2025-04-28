34c35bfc18e1165a39bab48cc9195453802d3384
Joshua Samraj <151136911+Joshua-Samraj@users.noreply.github.com>
2025-04-28 13:50:10 +0530

    Update urls.py

--- chatbot_app/urls.py
+++ chatbot_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path('chat/', views.chat_view, name='chat'),
     path('api/chat/', views.chat_api, name='chat_api'),
-    path('portfolio/', views.portfolio_view, name='portfolio'),
+    path('', views.portfolio_view, name='portfolio'),
 ]
